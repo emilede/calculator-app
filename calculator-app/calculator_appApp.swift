161a88da3bd9535b6909fa7cb5decdbb0bb47f8b
Emile de l'Isle <emiled@uoregon.edu>
2024-10-21 21:12:01 +0800

    Initial Commit

---
+++ calculator-app/calculator_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_appApp.swift
+//  calculator-app
+//
+//  Created by Emile de l'Isle on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct calculator_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ calculator-appTests/calculator_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  calculator_appTests.swift
+//  calculator-appTests
+//
+//  Created by Emile de l'Isle on 10/21/24.
+//
+
+import Testing
+@testable import calculator_app
+
+struct calculator_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
