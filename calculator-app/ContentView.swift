1a0b9247736f45ade316d38793ba209b7ab95e7d
Emile de l'Isle <emiled@uoregon.edu>
2024-10-21 21:46:26 +0800

    added UI for calc

--- calculator-app/ContentView.swift
+++ calculator-app/ContentView.swift
@@ -10,13 +10,187 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Spacer()
+            Spacer()
+            Spacer()
+            
+            HStack{
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "c.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "plusminus.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "divide.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+            }
+
+            HStack{
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "7.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "8.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "9.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "x.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                
+            }
+
+            HStack{
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "4.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "5.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "6.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "minus.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+
+            }
+            HStack{
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "1.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "2.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "3.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "plus.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+            }
+            
+            HStack{
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "0.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+                
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "dot.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+
+                Button(action: {
+                    // Action for button press
+                }) {
+                    Image(systemName: "equal.circle")
+                        .resizable()
+                        .frame(width: 75, height: 75)
+                }
+            }
+            
+        Spacer()
         }
-        .padding()
+        
     }
+
 }
 
 #Preview {
